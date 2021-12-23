61312cd736e17251bb68a8c949d7c2e20eeb3b08
heysalihugur <salih.ugur@heyteknoloji.com>
2021-12-23 22:48:24 +0300

    first commit :)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import { FaFacebookF, FaGoogle, FaGithub, FaRegEnvelope } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+import { FcGoogle } from "react-icons/fc";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -9,60 +11,95 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <div className="bg-white rounded-xl shadow-2xl w-2/3 max-w-4xl flex">
+          {/* Sign In */}
+          <div className="w-3/5 p-5">
+            <div className="text-left font-bold">
+              <span className="text-green-700 font-bold">NDB</span>Learning
+            </div>
+            <div className="py-10">
+              <div className="text-3xl font-bold text-gray-900">
+                <span className="text-3xl font-bold text-green-700 mb-2">
+                  Sign in to NDB
+                </span>
+                Learning
+              </div>
+              <div className="border-2 w-10 border-green-700 inline-block mb-2"></div>
+              <div className="flex justify-center my-2">
+                <a
+                  href="#"
+                  className="border-2 border-gray-100 rounded-full p-3 mx-1"
+                >
+                  <FaFacebookF className="text-2xl text-blue-900" />
+                </a>
+                <a
+                  href="#"
+                  className="border-2 border-gray-100 rounded-full p-3 mx-1"
+                >
+                  <FcGoogle className="text-2xl" />
+                </a>
+                <a
+                  href="#"
+                  className="border-2 border-gray-100 rounded-full p-3 mx-1"
+                >
+                  <FaGithub className="text-2xl" />
+                </a>
+              </div>
+              <p className="text-gray-400 my-3">or use your email account</p>
+              <div className="flex flex-col items-center mb-3">
+                <div className="bg-gray-100 w-64 p-2 flex items-center">
+                  <FaRegEnvelope className="text-gray-400 m-2" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className="bg-gray-100 outline-none"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col items-center mb-3">
+                <div className="bg-gray-100 w-64 p-2 flex items-center">
+                  <MdLockOutline className="text-gray-400 m-2" />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="bg-gray-100 outline-none"
+                  />
+                </div>
+                <div className="flex justify-between items-center mt-3 w-64 mb-5">
+                  <label className="text-xs items-center flex">
+                    <input type="checkbox" name="remember" className="mr-1" />
+                    Remember me
+                  </label>
+                  <a href="#" className="text-xs">
+                    Forgot Password?
+                  </a>
+                </div>
+                <a
+                  href="#"
+                  className="border-2 border-green-700 text-green-700 rounded-full px-12 py-2 inline-block font-semibold hover:font-bold hover:bg-green-700 hover:border-white hover:text-white"
+                >
+                  Sign in
+                </a>
+              </div>
+            </div>
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+          {/* Sign Up */}
+          <div className="w-2/5 bg-green-700 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+            <h2 className="text-3xl font-bold mb-2">Hello!</h2>
+            <div className="border-2 w-10 border-white inline-block mb-2"></div>
+            <p className="mb-10">
+              Fill up personal information and start journey with us!
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <a
+              href="#"
+              className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:font-bold hover:bg-white hover:border-white hover:text-green-700"
+            >
+              Sign Up
+            </a>
+          </div>
         </div>
       </main>
 
@@ -73,10 +110,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Powered by{" "}
+          <img src="/logo-gray.png" alt="Hitsoft Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
