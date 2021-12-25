b6faf3d6b98a836708c07fac998c6c1be8233b6d
heysalihugur <salih.ugur@heyteknoloji.com>
2021-12-25 19:31:16 +0300

    fixes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,4 +10,42 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, token, user }) {
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token;
+    },
+  },
+
+  events: {
+    async signIn(message) {
+      /* on successful sign in */
+    },
+    async signOut(message) {
+      /* on signout */
+    },
+    async createUser(message) {
+      /* user created */
+    },
+    async updateUser(message) {
+      /* user updated - e.g. their email was verified */
+    },
+    async linkAccount(message) {
+      /* account (e.g. Twitter) linked to a user */
+    },
+    async session(message) {
+      /* session is active */
+    },
+    async error(message) {
+      /* error in authentication flow */
+    },
+  },
 });
